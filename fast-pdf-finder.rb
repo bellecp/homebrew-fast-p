a6f321129d291c55c019521d1d2f7887790e0af7
bellecp <bellecp@users.noreply.github.com>
2018-09-16 22:32:02 -0400

    Brew formula update for fast-p version v0.2.2

--- fast-pdf-finder.rb
+++ fast-pdf-finder.rb
@@ -1,14 +1,15 @@
 class FastPdfFinder < Formula
   desc "Fast, command-line PDF finder"
   homepage "https://github.com/bellecp/fast-p"
-  url "https://github.com/bellecp/fast-p/releases/download/v0.2.1/fast-p_0.2.1_Darwin_x86_64.tar.gz"
-  version "0.2.1"
-  sha256 "029e8655ac9a3618fb476dfbc4b83f183620147c5442dca5d3df4b41a3fa7e41"
+  url "https://github.com/bellecp/fast-p/releases/download/v0.2.2/fast-p_0.2.2_Darwin_x86_64.tar.gz"
+  version "0.2.2"
+  sha256 "90c692cd1907761045d41870b40f0656c5f323b69c5f62557906c973a97b083e"
   
   depends_on "fzf"
   depends_on "coreutils"
   depends_on "findutils"
-  depends_on "poppler-utils"
+  depends_on "poppler"
+  depends_on "pkg-config"
   depends_on "the_silver_searcher"
 
   def install
